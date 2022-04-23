1e1894470c84b7bdd12fffc2bdfb6da67d57d874
Samuel Texier <stexier@et.esiea.fr>
2022-04-23 13:33:39 +0200

    Init projet with Spring framework with H2 Database

---
+++ DeltaProject/src/test/java/com/DeltaProjectEsiea/DeltaProject/DeltaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DeltaProjectEsiea.DeltaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
